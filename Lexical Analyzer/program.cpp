77aebd823c24db41f1a9a57f901be7093a44f4cf
zilanikhan-08 <134102450+zilanikhan-08@users.noreply.github.com>
2024-05-05 21:25:51 +0600

    Update program.cpp

--- Lexical Analyzer/program.cpp
+++ Lexical Analyzer/program.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <unordered_set>
 #include <sstream>
+#include <unistd.h>
 using namespace std;
 
-const int MAX_KEYWORDS = 50;
-const int MAX_TOKENS_PER_LINE = 100;
 const int MAX_LINE_LENGTH = 1000;
-const int MAX_HEADER_SIZE = 100;
 
 // Function to check if a character is alphanumeric
 bool isAlphanumeric(char c) {
@@ -25,60 +22,100 @@ bool isKeyword(const string& word, const string keywords[], int numKeywords) {
     return false;
 }
 
+// Function to check for the presence of the header file
+bool isHeader(const string& sourceCode, const string& header) {
+    int pointer = 0;
+    if (sourceCode[pointer] == '#' && sourceCode[pointer + 1] == 'i' && sourceCode[pointer + 2] == 'n' &&
+        sourceCode[pointer + 3] == 'c' && sourceCode[pointer + 4] == 'l' && sourceCode[pointer + 5] == 'u' &&
+        sourceCode[pointer + 6] == 'd' && sourceCode[pointer + 7] == 'e') {
+        pointer += 8;
+
+        while (sourceCode[pointer] == ' ' || sourceCode[pointer] == '\t') {
+            ++pointer;
+        }
+
+        if (sourceCode[pointer] == '<') {
+            ++pointer;
+
+            int k = 0;
+            while (sourceCode[pointer] != '\0') {
+                if (sourceCode[pointer] == header[k]) {
+                    ++pointer;
+                    ++k;
+                    if (sourceCode[pointer] == '>' && (header[k] == ',' || header[k] == '\0')) {
+                        cout << "Header File Found" << endl;
+                        return true;
+                    }
+                } else {
+                    while (header[k] != ',' && header[k] != '\0') {
+                        ++k;
+                    }
+                    if (header[k] == ',') {
+                        ++k;
+                    } else {
+                        cout << "No Such Header File Found!!!" << endl;
+                        return false;
+                    }
+                }
+            }
+        } else {
+            cout << "Error! '<' expected after '#include'" << endl;
+            return false;
+        }
+    }
+    return false;
+}
+
 // Function to tokenize a line of code
-int tokenizeLine(const string& line, string tokens[][MAX_TOKENS_PER_LINE]) {
+int tokenizeLine(const string& line, string* tokens) {
     int numTokens = 0;
     string token = "";
     stringstream ss(line);
 
     while (ss >> token) {
-        tokens[numTokens++][0] = token;
+        tokens[numTokens++] = token;
     }
 
     return numTokens;
 }
 
 int main() {
-    // Check for the presence of the header file
-    const char* sourceCode = "#include <iostream>";
-    bool headerResult = isHeader(sourceCode);
-    if (!headerResult) {
-        cout << "No Header file found." << endl;
-        return 1;
-    }
-
     // Load C++ keywords from file
-    ifstream keywordFile("keywords.txt");
+    ifstream keywordFile("Desktop/keyword");
     if (!keywordFile.is_open()) {
-        cout << "Error opening keywords file." << endl;
+        cout << "Error opening keyword file." << endl;
         return 1;
     }
 
-    string keywords[MAX_KEYWORDS];
+    string* keywords = new string[MAX_LINE_LENGTH];
     int numKeywords = 0;
     string keyword;
-    while (numKeywords < MAX_KEYWORDS && keywordFile >> keyword) {
+    while (numKeywords < MAX_LINE_LENGTH && keywordFile >> keyword) {
         keywords[numKeywords++] = keyword;
     }
     keywordFile.close();
 
     // Open lex_input.txt
-    ifstream inputFile("lex_input.txt");
+    ifstream inputFile("Desktop/lex_input.txt");
     if (!inputFile.is_open()) {
         cout << "Error opening lex_input file." << endl;
+        delete[] keywords;
         return 1;
+    } else {
+        cout << "Input file opened successfully." << endl;
     }
+
+    string header;
+    cout << "Enter the header to search for: ";
+    cin >> header;
 
     string line;
     int lineNumber = 1;
-    string tokens[MAX_LINE_LENGTH][MAX_TOKENS_PER_LINE];
+    string* tokens = new string[MAX_LINE_LENGTH];
     while (getline(inputFile, line)) {
-        // Check for syntax errors, wrong data types, and report them
-        if (line.find("itn") != string::npos) {
-            cout << "Syntax error: 'itn' instead of 'int' in line " << lineNumber << endl;
-        }
-        if (line.find("#") != string::npos) {
-            cout << "Syntax error: '#' is not a valid character in line " << lineNumber << endl;
+        // Check for the presence of the header file
+        if (isHeader(line, header)) {
+            cout << "Header file found in line " << lineNumber << endl;
         }
 
         // Tokenize the line
@@ -86,17 +123,7 @@ int main() {
         cout << "Tokenizing line " << lineNumber << ": ";
         // Categorically tokenize every element
         for (int i = 0; i < numTokens; ++i) {
-            if (isKeyword(tokens[i][0], keywords, numKeywords)) {
-                cout << tokens[i][0] << " is a keyword. ";
-            } else if (tokens[i][0] == "=" || tokens[i][0] == "+" || tokens[i][0] == "-" || tokens[i][0] == "*" || tokens[i][0] == "/" || tokens[i][0] == "%" || tokens[i][0] == "<" || tokens[i][0] == ">" || tokens[i][0] == "==" || tokens[i][0] == "!=" || tokens[i][0] == "<=" || tokens[i][0] == ">=") {
-                cout << tokens[i][0] << " is an operator. ";
-            } else if (tokens[i][0] == ";" || tokens[i][0] == "," || tokens[i][0] == "{" || tokens[i][0] == "}" || tokens[i][0] == "(" || tokens[i][0] == ")") {
-                cout << tokens[i][0] << " is a punctuation. ";
-            } else if (isAlphanumeric(tokens[i][0][0]) || tokens[i][0][0] == '_') {
-                cout << tokens[i][0] << " is an identifier. ";
-            } else {
-                cout << tokens[i][0] << " is unrecognized. ";
-            }
+            // Process tokens
         }
         cout << endl;
 
@@ -104,6 +131,9 @@ int main() {
     }
 
     inputFile.close();
+
+    delete[] keywords;
+    delete[] tokens;
 
     return 0;
 }
